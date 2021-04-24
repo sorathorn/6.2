33799269a3e54f0262cf31aec5553e0f73e1c23f
Best\Sorathorn <sorathorn.kh@mail.kmutt.ac.th>
2021-04-24 16:24:41 +0700

    no message

---
+++ 6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _6
+{
+    class Program
+    {
+        static void S (int x, char a)
+        {
+            for (int i = 1; i <= 1; i++)
+            {
+                int j;
+                if (i % 2 != 0)
+                j = int + 1;
+            else
+                    j = i;
+            for (int k = 0; k < j; k++)
+                    Console.Write("{0},a);
+                    Console.WruteLine();
+            }
+        }
+    }
+
+    static void Main()
+    {
+        char a;
+        a = char.Parse(Console.ReadLine());
+        int x;
+        x = int.Parse(Console.ReadLine());
+        S(x, a);
+    }
+}
+    
